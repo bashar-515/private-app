432a2392d215791ae6ab4f4c81073381b93cc583
Bashar Eid <bashar.eid@viam.com>
2025-11-13 00:43:11 -0500

    paste in Viam Apps scaffolding

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,35 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as VIAM from "@viamrobotics/sdk";
+import Cookies from "js-cookie";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let apiKeyId = "";
+let apiKeySecret = "";
+let host = "";
+let machineId = "";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+async function main() {
+  const opts: VIAM.ViamClientOptions = {
+    serviceHost: "https://app.viam.com",
+    credentials: {
+      type: "api-key",
+      payload: apiKeySecret,
+      authEntity: apiKeyId,
+    },
+  };
+
+  const client = await VIAM.createViamClient(opts);
+  const machine = await client.appClient.getRobot(machineId);
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  // Extract the machine identifier from the URL
+  let machineCookieKey = window.location.pathname.split("/")[2];
+  ({
+    apiKey: { id: apiKeyId, key: apiKeySecret },
+    machineId: machineId,
+    hostname: host,
+  } = JSON.parse(Cookies.get(machineCookieKey)!));
+
+  main().catch((error) => {
+    console.error("encountered an error:", error);
+  });
+});
